d4ab4cecece64d46d56c964a1ed85ae33f7445c5
RaniaDg12 <rania.doggaz@etudiant-enit.utm.tn>
2025-01-22 23:45:54 +0100

    Add error handling

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from app.config import Config
 from app.models import db
 from app.routes.task_routes import task_bp
@@ -10,4 +10,8 @@ def create_app():
     db.init_app(app)  # Initialize SQLAlchemy
     app.register_blueprint(task_bp, url_prefix='/tasks')  # Register routes
 
+    @app.errorhandler(405)
+    def method_not_allowed(error):
+        return jsonify({"error": "Invalid route"}), 405
+    
     return app
